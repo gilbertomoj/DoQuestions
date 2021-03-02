4461ba15f4f6512d0e54c4e425cc7685aad0a7ba
ggibamede <gibamedeirosgc@gmail.com>
2021-03-02 16:48:47 -0300

    feature(front): Uso de partials e pagina de perguntas

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-import express from 'express';
+const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");//Responsavel por traduzir os dados recebidos do front(formulario) num modelo utilizavel no backend(server)
 
 app.set('view engine','ejs');
+
 app.use(express.static('public'));
-const list = {
-    id : '1',
-    nome: 'Jose Silva',
-    idade : 19,
-    cep: 13231323,
-    cpf : 12031203
-}
+
+//Configuração do Body Parser
+app.use(bodyParser.urlencoded({extended:false}));//Comando necessario para a tradução do body-parser
+app.use(bodyParser.json());//Ler dados enviados como JSON
+
+
+//Rotas
 app.get("/",(req, res)=>{
    
     res.render("index");
 
 });
 
-app.get('/perguntar',(req, res)=>{
+app.get("/perguntar",(req, res)=>{
     res.render("perguntar");
+})
+
+app.post("/salvarpergunta", (req, res)=>{
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send('Formulario foi recebido! Titulo : '+titulo+" Descrição : "+descricao)
 })
 
 app.listen(8080,()=>{
