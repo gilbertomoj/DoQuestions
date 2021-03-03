baae3ffc003871a2300e339b2632e5880676155c
ggibamede <gibamedeirosgc@gmail.com>
2021-03-03 00:29:18 -0300

    feature(server): Criado model de perguntas

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");//Responsável por traduzir os dados recebidos do front(formulario) num modelo utilizavel no backend(server)
 const connection = require('./database/database');
+const PerguntaModel = require('./database/Pergunta')
 //Database
 connection.authenticate().then(()=>{
     console.log('Conexão realizada com sucesso')
 }).catch((err)=>{
     console.log('Um erro ocorreu '+err)
 })
-
+//Configuração do EJS
 app.set('view engine','ejs');
-
 app.use(express.static('public'));
-
 //Configuração do Body Parser
 app.use(bodyParser.urlencoded({extended:false}));//Comando necessário para a tradução do body-parser
 app.use(bodyParser.json());//Ler dados enviados como JSON
